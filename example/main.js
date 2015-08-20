c3c42f59aa3384cc455498538394bf723f7f30d0
speveril <speveril@gmail.com>
2015-08-19 23:09:43 -0700

    Add some actual usage of keys and stuff. Commit what I left out of the last commit.

--- example/main.js
+++ example/main.js
@@ -2,6 +2,23 @@ include('test.js');
 
 module.exports = {
     start: function() {
-        console.log("Starting my game");
+        var tex = Egg.loadTexture("res/sprite/lance-vodwin.png");
+        this.sprite = Egg.makeSprite(tex);
+        Egg.stage.addChild(this.sprite);
+    },
+
+    frame: function(dt) {
+        if (Egg.keys[37]) { // left
+            this.sprite.position.x--;
+        }
+        if (Egg.keys[38]) { // up
+            this.sprite.position.y--;
+        }
+        if (Egg.keys[39]) { // right
+            this.sprite.position.x++;
+        }
+        if (Egg.keys[40]) { // down
+            this.sprite.position.y++;
+        }
     }
 }
--- resources/default_app/egg.js
+++ resources/default_app/egg.js
@@ -18,7 +18,10 @@ function Egg(argsArray) {
 Egg.prototype = {
     run: function() {
         process.chdir(this.game);
+        this.gameDir = "../../" + this.game;
+        this.browserWindow = remote.getCurrentWindow();
 
+        // read/parse config
         try {
             this.config = JSON.parse(fs.readFileSync("config.json"));
         } catch(e) {
@@ -26,19 +29,30 @@ Egg.prototype = {
             window.close();
         }
 
-        this.config.width = this.config.width || 640;
-        this.config.height = this.config.height || 480;
+        this.config.width = this.config.width || 320;
+        this.config.height = this.config.height || 240;
 
-        window.resizeTo(this.config.width, this.config.height);
-        window.moveTo(screen.width / 2 - this.config.width / 2, screen.height / 2 - this.config.height / 2);
+        // set up window
+        this.browserWindow.setContentSize(this.config.width, this.config.height);
+        this.browserWindow.center();
+        this.browserWindow.setMinimumSize(this.config.width, this.config.height);
 
         window.addEventListener('keydown', this.onKeyDown.bind(this));
         window.addEventListener('keyup', this.onKeyUp.bind(this));
         window.addEventListener('resize', this.onResize.bind(this));
 
+        // set up graphics
+        PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
+        this.renderer = new PIXI.autoDetectRenderer(this.config.width, this.config.height);
+        this.renderer.backgroundColor = 0x888888;
+        document.body.appendChild(this.renderer.view);
+        this.stage = new PIXI.Container();
+
+        // set up animation loop
         this.lastTime = Date.now();
         this.frame();
 
+        // start the game
         this.Game = include("/main.js");
         this.Game.start();
     },
@@ -49,7 +63,11 @@ Egg.prototype = {
         var dt = Date.now() - this.lastTime;
         this.lastTime += dt;
 
-        // TODO do actual frame stuff
+        if (this.Game && this.Game.frame) {
+            this.Game.frame(dt);
+        }
+
+        this.renderer.render(this.stage);
     },
 
     onKeyDown: function(event) {
@@ -59,18 +77,36 @@ Egg.prototype = {
     onKeyUp: function(event) {
         this.keys[event.keyCode] = false;
 
+        // console.log(event.keyCode);
+
         // DEBUGGING KEYS
-        if (this.debug && event.keyCode === 192) {
+        if (this.debug && event.keyCode === 192) { // ~ key, opens console
             remote.getCurrentWindow().toggleDevTools();
         }
     },
 
     onResize: function(event) {
-        console.log("TODO resize handling");
+        var newSize = this.browserWindow.getContentSize(),
+            multX   = newSize[0] / this.config.width,
+            multY   = newSize[1] / this.config.height,
+            mult    = Math.floor(Math.min(multX, multY));
+        // console.log(newSize, multX, multY, mult, [this.config.width * mult, this.config.height * mult]);
+        this.renderer.resolution = mult;
+        // this.renderer.resize(this.config.width * mult, this.config.height * mult);
+        this.renderer.resize(this.config.width, this.config.height);
+        // console.log(this.renderer.view);
     },
 
     setTitle: function(title) {
         remote.getCurrentWindow().setTitle(title);
+    },
+
+    loadTexture: function(path) {
+        return PIXI.Texture.fromImage(this.gameDir + "/" + path);
+    },
+
+    makeSprite: function(tex) {
+        return new PIXI.Sprite(tex);
     },
 
     quit: function() {
