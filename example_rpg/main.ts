43dbc94c36d34a0a78aba65f7f482c9153a38705
speveril <speveril@gmail.com>
2015-09-05 16:14:31 -0700

    Add example_rpg (SimpleQuest)

---
+++ example_rpg/main.ts
@@ -0,0 +1,11 @@
+///<reference path="../resources/default_app/Egg.d.ts" />
+
+module SimpleQuest {
+    export function start() {
+    }
+
+    export function frame(dt) {
+    }
+}
+
+module.exports = SimpleQuest;
