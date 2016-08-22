75e9bcdf605832057e6f17a07b3a7af818a66e9f
speveril <speveril@gmail.com>
2016-08-21 17:48:28 -0700

    Wire up all of the doors, switches, and keys in the castle.

--- simplequest/map/castle.ts
+++ simplequest/map/castle.ts
@@ -7,14 +7,48 @@ module SimpleQuest {
 
         open() {
             super.open();
+
+            this.fixSwitchDoor('trigger_green_castle_switch', 'green_door');
+            this.fixSwitchDoor('trigger_red_castle_switch', 'red_door');
+            this.fixSwitchDoor('trigger_blue_castle_switch', 'blue_door');
+            this.fixKeyDoor('castle_door_A');
+            this.fixKeyDoor('castle_door_B');
         }
 
         exit_castle(args) {
             this.map_switch(new Map_Overworld(), 3, 6);
         }
 
-        trigger_green_castle_switch(args) {
+        castle_door_A(args) {
+            this.doKeyDoor('castle_door_A', 'iron_key');
+        }
 
+        castle_door_B(args) {
+            this.doKeyDoor('castle_door_B', 'steel_key');
+        }
+
+        trigger_green_castle_switch(args) {
+            this.doSwitch('trigger_green_castle_switch', 'green_door');
+        }
+
+        green_door(args) {
+            this.doSwitchDoor('green_door');
+        }
+
+        trigger_red_castle_switch(args) {
+            this.doSwitch('trigger_red_castle_switch', 'red_door');
+        }
+
+        red_door(args) {
+            this.doSwitchDoor('red_door');
+        }
+
+        trigger_blue_castle_switch(args) {
+            this.doSwitch('trigger_blue_castle_switch', 'blue_door');
+        }
+
+        blue_door(args) {
+            this.doSwitch('blue_door');
         }
     }
 }
