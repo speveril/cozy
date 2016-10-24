fe0a7fe13968362a04b0b296066d488640f94f39
speveril <speveril@gmail.com>
2016-10-23 21:01:23 -0700

    - Add functions to die rolling, and implement best() and worst()

--- simplequest/src/rpg/Dice.ts
+++ simplequest/src/rpg/Dice.ts
@@ -1,16 +1,29 @@
+// (best 4 of 4d8)
+
 module RPG {
-    var tokenRegexp = '\\(|\\)|d|\\*|\\/|\\+|\\-|\\$\\w+|\\d+';
-    var tokenDefs = {
-        LPAREN: '(',
-        RPAREN: ')',
-        D: /d/i,
-        MUL: '*',
-        DIV: '/',
-        ADD: '+',
-        SUB: '-',
-        VAR: /$\w+/,
-        NUM: /\d+/
-    }
+    // var dbg = console.log.bind(console); // uncomment to turn ON debugging
+    var dbg = function(...args) {}; // uncomment to turn OFF debugging
+
+    var tokenRegexp = '\\(|\\)|d|\\*|\\/|\\+|\\-|\\$\\w+|\\w+|\\d+|,';
+    // var tokenDefs = {
+    //     LPAREN: '(',
+    //     RPAREN: ')',
+    //     D: /d/i,
+    //     MUL: '*',
+    //     DIV: '/',
+    //     ADD: '+',
+    //     SUB: '-',
+    //     VAR: /$\w+/,
+    //     NUM: /\d+/
+    // };
+
+    var rollDie = (dieSize) => Math.floor(Math.random() * dieSize) + 1;
+    var rollDice = (numDice, dieSize) => {
+        var rolls = [];
+        _.times(numDice, () => rolls.push(rollDie(dieSize)));
+        return rolls;
+    };
+
 
     export class Dice {
         actor:RPG.Character;
@@ -54,8 +67,8 @@ module RPG {
                     t.type = 'LPAREN';
                 } else if (ch === ')') {
                     t.type = 'RPAREN';
-                } else if (ch === 'd' || ch === 'D') {
-                    t.type = 'D';
+                } else if (ch === ',') {
+                    t.type = 'COMMA';
                 } else if (ch === '*' || ch === '/') {
                     t.type = 'MUL';
                 } else if (ch === '+' || ch === '-') {
@@ -67,6 +80,10 @@ module RPG {
                 } else if (ch.match(/\d/)) {
                     t.type = 'NUM';
                     t.value = getWord(i, /\d/);
+                } else if (ch.match(/[a-zA-Z]/)) {
+                    t.value = getWord(i, /[a-zA-Z_]+/);
+                    if (t.value === 'd' || t.value === 'D') t.type = 'D';
+                    else t.type = 'IDENT';
                 }
 
                 if (!t.type) {
@@ -88,11 +105,13 @@ module RPG {
         }
 
         parse() {
+            dbg("parse:", this.tokens);
             this.parseIndex = 0;
             var ex = this.parseExpr();
             if (this.parseIndex < this.tokens.length) {
                 throw new Error("Parse error, expected end of expression at " + this.peek().value);
             }
+            dbg("expr -> ", ex);
             return ex;
         }
 
@@ -104,6 +123,7 @@ module RPG {
                 var rhs = this.parseMUL();
                 expr = { type: t.value === '+' ? "add" : 'subtract', lhs:expr, rhs:rhs };
                 t = this.peek();
+                dbg("add ->",expr);
             }
 
             return expr;
@@ -117,6 +137,7 @@ module RPG {
                 var rhs = this.parseD();
                 expr = { type: t.value === '*' ? "multiply" : 'divide', lhs:expr, rhs:rhs };
                 t = this.peek();
+                dbg("mul ->",expr);
             }
             return expr;
         }
@@ -128,6 +149,7 @@ module RPG {
                 this.consume();
                 var rhs = this.parsePrime();
                 expr = { type: "roll", lhs:expr, rhs:rhs };
+                dbg("d ->",expr);
             }
             return expr;
         }
@@ -136,9 +158,11 @@ module RPG {
             var t = this.peek();
             if (t.type === 'NUM') {
                 this.consume();
+                dbg("(num) ->", t.value);
                 return { type:"number", value: parseInt(t.value,10) };
             } else if (t.type === 'VAR') {
                 this.consume();
+                dbg("(var) ->")
                 return { type:"variable", value: t.value.slice(1) };
             } else if (t.type === 'LPAREN') {
                 this.consume();
@@ -147,9 +171,28 @@ module RPG {
                     throw new Error("Unbalanced parens, expected ).");
                 }
                 this.consume();
+                dbg('subexpr ->', expr);
                 return expr;
+            } else if (t.type === 'IDENT') {
+                this.consume();
+                var params = [];
+                if (this.peek().type != 'LPAREN') throw new Error("Identifier must be followed by an expression list.");
+                this.consume();
+                while (true) {
+                    params.push(this.parseExpr());
+                    if (this.peek().type === 'RPAREN') {
+                        this.consume();
+                        break;
+                    } else if (this.peek().type === 'COMMA') {
+                        this.consume();
+                    } else {
+                        throw new Error("Unfinished function call (next token is " + this.peek().type + ")");
+                    }
+                }
+                dbg(t.value,'->',params);
+                return { type:"func", name:t.value, value:params };
             } else {
-                throw new Error("Expected number, var, or paren.");
+                throw new Error("Expected identifier, number, var, or paren.");
             }
         }
 
@@ -158,6 +201,7 @@ module RPG {
         }
 
         consume() {
+            dbg(" >> CONSUME", this.tokens[this.parseIndex]);
             this.parseIndex++;
         }
 
@@ -189,20 +233,47 @@ module RPG {
                     result = this.resolveNode(actor, node.lhs) / this.resolveNode(actor, node.rhs);
                     break;
                 case 'roll':
-                    var numRolls = this.resolveNode(actor, node.lhs);
-                    var dieSize = this.resolveNode(actor, node.rhs);
-                    var rolls = [];
-                    _.times(numRolls, () => {
-                        // TODO collapse this, it's only necessary to save the rolls for debugging
-                        rolls.push(Math.floor(Math.random() * dieSize) + 1)
-                        result += rolls[rolls.length - 1];
-                    });
-                    // console.log("#", numRolls + "d" + dieSize + ":", rolls.join(" "));
+                    result = this.func_roll(actor, [node.lhs, node.rhs]);
+                    break;
+                case 'func':
+                    dbg("FUNC: ", node.name, node.value);
+                    var func = this['func_' + node.name];
+                    if (func === undefined) throw new Error("Called unrecognized function " + node.name + "()");
+                    result = func.call(this, actor, node.value);
                     break;
                 default:
                     throw new Error("Unrecognized node in AST: " + node.type);
             }
 
+            return result;
+        }
+
+        func_roll(actor:RPG.Character, args:Array<any>):number {
+            var rolls = rollDice(
+                <number>this.resolveNode(actor, args[0]),
+                <number>this.resolveNode(actor, args[1]));
+            var result = _.reduce(rolls, (accum:number, num:number) => accum + num);
+            dbg(rolls,"->",result);
+            return result;
+        }
+
+        func_best(actor:RPG.Character, args:Array<any>) {
+            if (args[1].type !== 'roll') throw new Error("Parameter 2 to best() must be a roll.");
+            var best:number = <number>this.resolveNode(actor, args[0]);
+            var rolls = rollDice(this.resolveNode(actor, args[1].lhs), this.resolveNode(actor, args[1].rhs));
+            rolls.sort();
+            var result = _.reduce(rolls.slice(-best), (accum:number, num:number) => accum + num);
+            dbg(rolls,"->",result);
+            return result;
+        }
+
+        func_worst(actor:RPG.Character, args:Array<any>) {
+            if (args[1].type !== 'roll') throw new Error("Parameter 2 to best() must be a roll.");
+            var worst:number = <number>this.resolveNode(actor, args[0]);
+            var rolls = rollDice(this.resolveNode(actor, args[1].lhs), this.resolveNode(actor, args[1].rhs));
+            rolls.sort();
+            var result = _.reduce(rolls.slice(0, worst), (accum:number, num:number) => accum + num);
+            dbg(rolls,"->",result);
             return result;
         }
     }
