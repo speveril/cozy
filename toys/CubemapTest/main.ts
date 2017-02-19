fe9d15252a6a63fa4acd1d3e12d3f566df7862d5
speveril <speveril@gmail.com>
2017-02-19 15:43:36 -0800

    - Work a little bit more on the cube-tile/iso engine toy.

--- toys/CubemapTest/main.ts
+++ toys/CubemapTest/main.ts
@@ -1,28 +1,64 @@
 module CubemapTest {
-    var maplayout = [
+    let maplayout = [
         [[1, 1, 1], [1, 1, 1], [1, 0, 1]],
-        [[1, 1, 0], [1, 0, 0], [0, 0, 0]],
+        [[1, 0, 0], [1, 0, 0], [0, 0, 0]],
         [[1, 1, 0], [0, 0, 0], [0, 0, 0]],
     ];
-    var mapsize = [3, 3, 3];
-
+    let mapsize = [3, 3, 3];
+    let playerPosition = [ 0.5, 0.5, 1 ];
+    let playerSprite;
+    let plane:Cozy.RenderPlane;
+    let layer:Cozy.Layer;
 
     export function start() {
         Cozy.loadTextures({
-            cube_1: 'assets/cube1.png'
+            cube_1:             new Cozy.File('assets/cube-big-001.png'),
+            sprite_kiel:        new Cozy.File('assets/sprite-kiel.png')
         }).then(setup);
     }
 
     export function frame(dt) {
+        if (playerSprite) {
+            let dx = 0, dy = 0, dz = 0;
+            if (Cozy.Input.pressed('up')) { dx--; dy--; }
+            if (Cozy.Input.pressed('right')) { dx++; dy--; }
+            if (Cozy.Input.pressed('down')) { dx++; dy++; }
+            if (Cozy.Input.pressed('left')) { dx--; dy++; }
+            if (Cozy.Input.pressed('confirm')) { dz++; }
+            if (Cozy.Input.pressed('cancel')) { dz--; }
+
+            playerPosition[0] += dx * dt;
+            playerPosition[1] += dy * dt;
+            playerPosition[2] += dz * dt;
+
+            playerSprite['3dpos'] = playerPosition;
+            playerSprite.setPosition(
+                playerPosition[0] * 60 - playerPosition[1] * 60,
+                playerPosition[0] * 45 + playerPosition[1] * 45 - playerPosition[2] * 60
+            );
+        }
+
+        layer.sortSprites((a,b) => {
+            return (a['3dpos'][0] + a['3dpos'][1] + a['3dpos'][2] * 0.000001) - (b['3dpos'][0] + b['3dpos'][1] + b['3dpos'][2] * 0.000001);
+        });
     }
 
     function setup() {
-        var plane:Cozy.RenderPlane = <Cozy.RenderPlane>Cozy.addPlane(Cozy.RenderPlane);
-        var layer:Cozy.Layer = plane.addRenderLayer();
-
+        plane = <Cozy.RenderPlane>Cozy.addPlane(Cozy.RenderPlane);
+        layer = plane.addRenderLayer()
         layer.offset(960, 540);
 
-        var x = 0, y = 0, z = 0;
+        let x = 0, y = 0, z = 0;
+
+        playerSprite = new Cozy.Sprite({
+            texture: Cozy.textures['sprite_kiel'],
+            hotspot: { x: 60, y: 100 },
+            position: {
+                x: playerPosition[0] * 60 - playerPosition[1] * 60,
+                y: playerPosition[0] * 45 + playerPosition[1] * 45 - playerPosition[2] * 60
+            }
+        });
+        layer.add(playerSprite);
 
         while (true) {
             z++;
@@ -44,17 +80,20 @@ module CubemapTest {
                 continue;
             }
 
-            console.log(x,y,z);
-            var tile = maplayout[z][y][x];
+            // console.log(x,y,z);
+            let tile = maplayout[z][y][x];
             if (tile !== 0) {
-                layer.add(new Cozy.Sprite({
+                let s = new Cozy.Sprite({
                     texture: Cozy.textures['cube_' + tile],
-                    hotspot: { x: 48, y: 54 },
+                    hotspot: { x: 60, y: 150 },
                     position: {
-                        x: x * 48 - y * 48,
-                        y: x * 24 + y * 24 - z * 30
+                        x: (x + 0.5) * 60 - (y + 0.5) * 60,
+                        y: (x + 0.5) * 45 + (y + 0.5) * 45 - (z + 0.5) * 60
                     }
-                }))
+                });
+                s['3dpos'] = [x + 0.5, y + 0.5, z];
+                s['sortKey'] =
+                layer.add(s);
             }
         }
 
