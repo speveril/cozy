024d7c15e0481daf94b6b5ed8eb907fa978bd494
speveril <speveril@gmail.com>
2019-03-02 11:48:06 -0800

    Remove unused .bat file.

--- set-exe-strings.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set RCEDIT=tool\rcedit.exe
-%RCEDIT% %1 --set-icon cozy.ico
-%RCEDIT% %1 --set-version-string "FileDescription" "Cozy Engine"
-%RCEDIT% %1 --set-version-string "ProductName" "Cozy Engine"
-%RCEDIT% %1 --set-version-string "LegalCopyright" "Copyright (C) 2017 Shamus Peveril"
-%RCEDIT% %1 --set-version-string "OriginalFilename" "cozy.exe"
